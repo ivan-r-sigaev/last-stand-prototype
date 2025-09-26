2c20cd32f5e46120fd171dd48e983160d64a2d81
Ivan <98910154+ivan-r-sigaev@users.noreply.github.com>
2025-09-26 20:26:15 +0300

    Add macroquad crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    todo!()
+use macroquad::prelude as mq;
+
+#[macroquad::main("Last Stand")]
+async fn main() {
+    // Sample code
+    loop {
+        mq::clear_background(mq::RED);
+
+        mq::draw_line(40.0, 40.0, 100.0, 200.0, 15.0, mq::BLUE);
+        mq::draw_rectangle(mq::screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, mq::GREEN);
+        mq::draw_circle(mq::screen_width() - 30.0, mq::screen_height() - 30.0, 15.0, mq::YELLOW);
+        mq::draw_text("HELLO", 20.0, 20.0, 20.0, mq::DARKGRAY);
+
+        mq::next_frame().await
+    }
 }
