e594ef11f761b6e0f48c94110b68edd45c868c74
Ivan <98910154+ivan-r-sigaev@users.noreply.github.com>
2025-09-28 15:30:43 +0300

    Add letterbox sample code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![allow(clippy::await_holding_refcell_ref)]
+use std::{
+    any::{Any, TypeId},
+    cell::Ref,
+};
+
+use macroquad::{color::Color, math::Vec2};
+
+use crate::{
+    collision::Collider,
+    ecs::entity::World,
+    rendering::{Screen, Sprite},
+};
+
 #[allow(unused)]
 mod collision;
 #[allow(unused)]
@@ -5,7 +19,30 @@ mod ecs;
 #[allow(unused)]
 mod rendering;
 
+// const PHYSICS_DELTA: f32 = 1. / 60.;
+
 #[macroquad::main("Last Stand")]
 async fn main() {
-    todo!()
+    let mut world = World::new();
+    world.register_type::<Collider>();
+    world.register_type::<Sprite>();
+    // let mut collisions = CollisionGrid::new();
+    // let mut physics_time = 0.;
+    let screen = Screen::new(1024, 768);
+    let sprited_guard = world.pool(TypeId::of::<Sprite>()).unwrap().borrow() as Ref<'_, dyn Any>;
+    let sprites = sprited_guard.downcast_ref().unwrap();
+    let colliders_guard =
+        world.pool(TypeId::of::<Collider>()).unwrap().borrow() as Ref<'_, dyn Any>;
+    let colliders = colliders_guard.downcast_ref().unwrap();
+    loop {
+        screen
+            .render_sprites(
+                Vec2::ZERO,
+                Vec2::ONE,
+                Color::from_hex(0xffb30f),
+                sprites,
+                colliders,
+            )
+            .await;
+    }
 }
